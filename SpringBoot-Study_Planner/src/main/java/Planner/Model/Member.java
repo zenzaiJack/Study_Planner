8f543e38c0f85d434355b46990d765883919b798
User <User@DESKTOP-1HS8I8P>
2023-04-20 11:35:02 +0900

    test

--- SpringBoot-Study_Planner/src/main/java/Planner/Model/Member.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/Model/Member.java
@@ -1,11 +1,15 @@
 package Planner.Model;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class Member {
 	private String member_id;
 	private String password;
-	private String name;
+	private String nickname;
 	private String email;
+	private Date create_time;
+	
 }
