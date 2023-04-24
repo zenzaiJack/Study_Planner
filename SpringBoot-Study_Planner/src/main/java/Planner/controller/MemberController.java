25d23efa75477b2ae3c706cf15c640b2cd6debc8
설재상 <@LAPTOP-9MEEHOQG>
2023-04-24 17:27:24 +0900

    bootstrap4

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/MemberController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/MemberController.java
@@ -1,5 +1,23 @@
 package Planner.controller;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MemberController {
+	@GetMapping("login")
+	public String login(Model model) {
+		return "login";
+	}
+	
+	@GetMapping("forget-password")
+	public String forgotPassword(Model model) {
+		return "forgot-password";
+	}
+	
+	@GetMapping("register")
+	public String register(Model model) {
+		return "register";
+	}
 }
