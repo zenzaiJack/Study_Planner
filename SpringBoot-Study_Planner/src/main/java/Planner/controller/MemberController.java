6874e205dfaaa72f0d0e7755c1fe5d5a74f33f6d
minho <minho@ASUS_ROGSTRIX>
2023-04-25 09:49:23 +0900

    model 부분 작성 완료 참조 부탁드립니다.

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/MemberController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/MemberController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
-	@GetMapping("login")
+	@GetMapping("login")	// 로그인
 	public String login(Model model) {
 		return "login";
 	}
 	
-	@GetMapping("forget-password")
+	@GetMapping("forget-password")	// 비밀번호 찾기
 	public String forgotPassword(Model model) {
 		return "forgot-password";
 	}
 	
-	@GetMapping("register")
+	@GetMapping("register")		// 회원가입
 	public String register(Model model) {
 		return "register";
 	}
