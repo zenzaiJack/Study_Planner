3846040545e8058d85e46db7217f7bc9e5fbbc22
User <User@DESKTOP-1HS8I8P>
2023-04-25 09:20:20 +0900

    planner.Html

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/PlannerController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/PlannerController.java
@@ -1,5 +1,18 @@
 package Planner.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("planner")
+@Controller
 public class PlannerController {
+	
+	
 	      
 }
