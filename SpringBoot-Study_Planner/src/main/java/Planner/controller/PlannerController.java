57b08b615e480849f32eb02cde0641a2ff86875d
박휘빈 <@WIN-9E0OVJ8KI61>
2023-04-25 09:20:56 +0900

    Plannercontroller

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/PlannerController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/PlannerController.java
@@ -1,5 +1,15 @@
 package Planner.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("plannner")
+@Controller
 public class PlannerController {
-	      
+	
 }
