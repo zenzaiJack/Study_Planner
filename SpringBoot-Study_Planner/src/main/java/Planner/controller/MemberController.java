1ff01a1e7d9032888783c8c398b8fd8fc7addfc8
설재상 <@LAPTOP-9MEEHOQG>
2023-04-25 09:54:23 +0900

    충돌발생

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/MemberController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/MemberController.java
@@ -1,16 +1,32 @@
 package Planner.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import Planner.Model.Member;
 
 @Controller
 public class MemberController {
 	@GetMapping("login")
 	public String login(Model model) {
+//		model.addAttribute("loginForm", new LoginForm());
 		return "login";
 	}
 	
+	@PostMapping("login")
+//	public String login(@ModelAttribute("loginForm") LoginForm loginForm, BindingResult result) {
+		
+//	}
+	
+	//일단 지금은 사용하지 않을 예정입니다.
 	@GetMapping("forget-password")
 	public String forgotPassword(Model model) {
 		return "forgot-password";
@@ -18,6 +34,22 @@ public class MemberController {
 	
 	@GetMapping("register")
 	public String register(Model model) {
+//		model.addAttribute("registerForm", new RegisterForm());
 		return "register";
+	}
+	
+//	@PostMapping("register")
+//	public String register(@ModelAttribute("registerForm") RegisterForm registerForm,
+//						   BindingResult result) {
+//		
+//	}
+	
+	@GetMapping("logout")
+	public String register(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		if(session != null) {
+			session.invalidate();
+		}
+		return "redirect:/";
 	}
 }
