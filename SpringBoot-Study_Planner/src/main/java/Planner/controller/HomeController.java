4642eff6a9b6611b489558445ff081bad5d9b4da
User <User@DESKTOP-1HS8I8P>
2023-04-25 10:12:46 +0900

    why don't this

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/HomeController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/HomeController.java
@@ -1,5 +1,15 @@
 package Planner.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	 @GetMapping("")
+	    public String home() {
+	        return "index";
+	    }
 }
