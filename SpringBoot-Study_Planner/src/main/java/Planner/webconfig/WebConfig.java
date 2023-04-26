ec2a182bfb7ebf7bdb7481cdd1cfaf3f4fa1673c
설재상 <@LAPTOP-9MEEHOQG>
2023-04-26 09:53:10 +0900

    add intercepter

---
+++ SpringBoot-Study_Planner/src/main/java/Planner/webconfig/WebConfig.java
@@ -0,0 +1,24 @@
+package Planner.webconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import Planner.intercepter.LoginCheckIntercepter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+    private String[] excludePaths = {"forgot-password", "/register", "/login", "/logout", 
+    								"/css/**",  "/js/**", "/scss/**", "/vendor/**","/*.ico", "/error"};
+
+	public void addIntercepters(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoginCheckIntercepter())
+				.order(1)
+				.addPathPatterns("/**")
+				.excludePathPatterns(excludePaths);
+	}
+	
+	
+}
