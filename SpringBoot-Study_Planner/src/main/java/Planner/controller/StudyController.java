ced887efedd5a8587a2d1a3e84244d41c01f7242
minho <minho@ASUS_ROGSTRIX>
2023-04-27 19:44:35 +0900

    일부 수정

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/StudyController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/StudyController.java
@@ -1,41 +1,25 @@
 package Planner.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import Planner.Model.Timer;
 import Planner.repository.TimerMapper;
 
-@Controller
+@RestController
 public class StudyController {
 	
-	@Autowired
-    private TimerMapper timerMapper;
-    
-    @RequestMapping(value = "/studyTimeList/{member_id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String studyTimeList(@PathVariable String member_id, HttpServletResponse response) {
-        List<Timer> list = timerMapper.personStudy(member_id);
-        List<String> array = new ArrayList<>();
-        for(int i = list.size() - 1; i >= Math.max(0, list.size()-7); i--) {
-            String study_date = list.get(i).getStudy_date();
-            array.add(study_date);
-        }
-        JSONArray jsonArray = new JSONArray(array);
-        String jsonStr = jsonArray.toString();
-        response.setContentType("application/json");
-        return jsonStr;
-    }
+	 @Autowired
+	    private TimerMapper timerMapper;
+
+	    @GetMapping("/study-time-list")
+	    public List<Timer> getStudyTimeList(@RequestParam("member_id") String memberId) {
+	        List<Timer> list = timerMapper.personStudy(memberId);
+	        return list.subList(Math.max(list.size() - 7, 0), list.size());
+	    }
 }
-	
 
