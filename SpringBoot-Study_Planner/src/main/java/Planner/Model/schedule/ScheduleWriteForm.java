04b2224256ac1dfd4cf3d3953951aee535adabd9
User <User@DESKTOP-1HS8I8P>
2023-05-02 18:34:56 +0900

    schedule add

--- SpringBoot-Study_Planner/src/main/java/Planner/Model/schedule/ScheduleWriteForm.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/Model/schedule/ScheduleWriteForm.java
@@ -1,26 +1,28 @@
 package Planner.Model.schedule;
 
-
-
-
-
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import lombok.Data;
 
 @Data
 public class ScheduleWriteForm {
+	@NotBlank
 	private String title;
+	@NotBlank
 	private String subject;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	 @DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDateTime start_date;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	 @DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDateTime end_date;
-	@DateTimeFormat(pattern = "aa hh:mm")
+	 @DateTimeFormat(pattern = "aa hh:mm")
 	private LocalDateTime start_time;
-	@DateTimeFormat(pattern = "aa hh:mm")
+	 @DateTimeFormat(pattern = "aa hh:mm")
 	private LocalDateTime end_time;
 	
 	public static Schedule toSchedule(ScheduleWriteForm scheduleWriteForm) {
@@ -28,9 +30,8 @@ public class ScheduleWriteForm {
 		schedule.setTitle(scheduleWriteForm.getTitle());
 		schedule.setSubject(scheduleWriteForm.getSubject());
 		schedule.setStart_date(scheduleWriteForm.getStart_date());
-		schedule.setEnd_date(scheduleWriteForm.getEnd_date());
-		schedule.setStart_time(scheduleWriteForm.getStart_time());
-		schedule.setEnd_time(scheduleWriteForm.getEnd_time());
+		schedule.setEnd_date(scheduleWriteForm.end_date);
+		schedule.setStart_time(scheduleWriteForm.start_time);
 		return schedule;
 	}
 }
