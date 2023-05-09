44a0453105b5b7e95ef385c3565c2f94e8be6ff7
minho <minho@ASUS_ROGSTRIX>
2023-05-09 09:15:21 +0900

    Merge branch 'master' of https://github.com/zenzaiJack/Study_Planner.git

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/ScheduleController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/ScheduleController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -17,10 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -41,62 +37,30 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class ScheduleController {
 
-	
-	private final ScheduleMapper scheduleMapper;
-	@GetMapping("month")		
-	public String monthForm(Model model) {
-		model.addAttribute("month", new Schedule());	
-		return "schedule/month";
-	}
-	
-	
-	@GetMapping("week")		
-	public String weekForm(Model model) {
-		model.addAttribute("week", new Schedule());	
-		return "schedule/week";
-	}
-	
-<<<<<<< HEAD
-//	@PostMapping("week")
-//	public String week(@SessionAttribute(value = "loginMember", required = false) Member loginMember,
-//            @Validated @ModelAttribute("weekForm")ScheduleWriteForm scheduleWriteForm,
-//            BindingResult result) {
-//		 // 로그인 상태가 아니면 로그인 페이지로 보낸다.
-//        if (loginMember == null) {
-//            return "redirect:/member/login";
-//        }
-//
-//        log.info("weekForm: {}", scheduleWriteForm);
-//        // validation 에러가 있으면 board/write.html 페이지를 다시 보여준다.
-//        if (result.hasErrors()) {
-//            return "schedule/week";
-//        }
-//        //날짜+시간 합치기
-//        LocalDateTime start_datetime = LocalDateTime.of(scheduleWriteForm.getStart_date().toLocalDate(), scheduleWriteForm.getStart_time().toLocalTime());
-//        LocalDateTime end_datetime = LocalDateTime.of(scheduleWriteForm.getEnd_date().toLocalDate(), scheduleWriteForm.getEnd_time().toLocalTime());
-//        scheduleWriteForm.setStart_date(start_datetime);
-//        scheduleWriteForm.setEnd_date(end_datetime);
-//        Schedule schedule = ScheduleWriteForm.toSchedule(scheduleWriteForm);
-//        schedule.setMember_id(loginMember.getMember_id());
-//        scheduleMapper.saveSchedule(schedule);
-//		return "schedule/week";
-//	}
-	
-	@PostMapping
-	@RequestMapping(value="sendSchedule", method=RequestMethod.POST)
-	public @ResponseBody int sendSchdule(@RequestBody Object obj) {
-		log.info("obj: {}", obj);
-		return 1;
-=======
-	@PostMapping("week")
-	public String week(@SessionAttribute(value = "loginMember", required = false) Member loginMember,
+   
+   private final ScheduleMapper scheduleMapper;
+   @GetMapping("month")      
+   public String monthForm(Model model) {
+      model.addAttribute("month", new Schedule());   
+      return "schedule/month";
+   }
+   
+   
+   @GetMapping("week")      
+   public String weekForm(Model model) {
+      model.addAttribute("week", new Schedule());   
+      return "schedule/week";
+   }
+   
+   @PostMapping("week")
+   public String week(@SessionAttribute(value = "loginMember", required = false) Member loginMember,
             @Validated @ModelAttribute("weekForm")ScheduleWriteForm scheduleWriteForm,
             BindingResult result, HashMap<String, String> param) {
-		 // 로그인 상태가 아니면 로그인 페이지로 보낸다.
+       // 로그인 상태가 아니면 로그인 페이지로 보낸다.
 //        if (loginMember == null) {
 //            return "redirect:/member/login";
 //        }
-		log.info("param: {}", param);
+      log.info("param: {}", param);
         log.info("ScheduleWriteForm: {}", scheduleWriteForm);
         // validation 에러가 있으면 board/write.html 페이지를 다시 보여준다.
         if (result.hasErrors()) {
@@ -112,9 +76,8 @@ public class ScheduleController {
         log.info("loginMember.getMember_id : {}", loginMember.getMember_id());
         scheduleMapper.saveSchedule(schedule);
         log.info("Schedule: {}", schedule);
-		return "schedule/week";
->>>>>>> branch 'master' of https://github.com/zenzaiJack/Study_Planner.git
-	}
+      return "schedule/week";
+   }
 
-	
+   
 }
