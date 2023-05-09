7b95f654f3fab83bf48337a798eac2ae9ea2e392
minho <minho@ASUS_ROGSTRIX>
2023-05-09 09:22:23 +0900

    chartcontroller

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/ChartController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/ChartController.java
@@ -29,7 +29,6 @@ public class ChartController {
 	        List<String> labels = new ArrayList<>();
 	        List<Integer> values = new ArrayList<>();
 	        List<Timer> timers = timerMapper.selectTimersByMemberId(loginMember.getMember_id());
-
 	        // DB에서 가져온 Timer 객체를 이용해 labels와 values 리스트에 데이터 추가
 	        for (int i = timers.size() - 1; i >= 0 && i >= timers.size() - 7; i--) {
 	            Timer timer = timers.get(i);
