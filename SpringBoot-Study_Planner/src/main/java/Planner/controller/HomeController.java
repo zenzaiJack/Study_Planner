bdc74b3e2a1c1969c462fc70969859a7fdc6265c
minho <minho@ASUS_ROGSTRIX>
2023-05-11 11:30:48 +0900

    Merge branch 'master' of https://github.com/zenzaiJack/Study_Planner.git

--- SpringBoot-Study_Planner/src/main/java/Planner/controller/HomeController.java
+++ SpringBoot-Study_Planner/src/main/java/Planner/controller/HomeController.java
@@ -1,5 +1,8 @@
 package Planner.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -11,17 +14,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import Planner.Model.member.Member;
-
-
+import Planner.Model.timer.Timer;
+import Planner.Model.timer.TimerForm;
+import Planner.repository.TimerMapper;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class HomeController {
 	
+	private TimerMapper timerMapper;
+	
 	@GetMapping("index")
 	public String index(@SessionAttribute(name = "loginMember", required = false) Member loginMember, Model model) {
-		return "working/index";
+		List<String> subject = new ArrayList<>();
+		List<String> subjects = timerMapper.getSubjectList(loginMember.getMember_id());
+	    log.info("subjects : {}", subjects);
+	    model.addAttribute("subjects", subjects);
+	    TimerForm timerForm = new TimerForm();
+	    timerForm.setMember_id(loginMember.getMember_id());
+	    model.addAttribute("timerForm", timerForm);
+	    return "working/index";
+	    
+	    
+        
+       
 	}
 }
 
